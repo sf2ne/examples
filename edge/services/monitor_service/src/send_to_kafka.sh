e57379161dc21ec3ae6892af9ee5d970bea74bbf
Sadiyah <sf2ne@virginia.ed>
2019-05-03 20:24:47 -0400

    refactoring for env vars for kafka

--- edge/services/monitor_service/src/send_to_kafka.sh
+++ edge/services/monitor_service/src/send_to_kafka.sh
@@ -4,11 +4,11 @@
 
 curl -sS 'http://camera/api/get_snippet' > bytes.txt
 
-kafkacat  -b "${SAMPLE_BROKER}" -P \
+kafkacat  -b "${MSGHUB_BROKER_URL}" -P \
     -X api.version.request=true \
     -X security.protocol=sasl_ssl \
     -X sasl.mechanisms=PLAIN \
-    -X sasl.username=${SAMPLE_APIKEY:0:16} \
-    -X sasl.password="${SAMPLE_APIKEY:16}" \
-    -t "${SAMPLE_TOPIC}" bytes.txt
+    -X sasl.username=${MSGHUB_API_KEY:0:16} \
+    -X sasl.password="${MSGHUB_API_KEY:16}" \
+    -t "${MSGHUB_TOPIC}" bytes.txt
 
