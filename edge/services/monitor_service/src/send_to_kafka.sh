6c3432b5a27300718a050f313f23dd54d18851f5
Sadiyah <sf2ne@virginia.ed>
2019-05-03 20:12:17 -0400

    removing hardcoded kafka creds

--- edge/services/monitor_service/src/send_to_kafka.sh
+++ edge/services/monitor_service/src/send_to_kafka.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-export SAMPLE_BROKER='kafka04-prod02.messagehub.services.us-south.bluemix.net:9093,kafka02-prod02.messagehub.services.us-south.bluemix.net:9093,kafka01-prod02.messagehub.services.us-south.bluemix.net:9093,kafka05-prod02.messagehub.services.us-south.bluemix.net:9093,kafka03-prod02.messagehub.services.us-south.bluemix.net:9093'
-export SAMPLE_APIKEY='xvj55vVXrVIdYX1pCVPLOya2nTHNSEnF4BHLimq5wMPg4zvg'
-export SAMPLE_TOPIC='Sadiyah.Faruk_ibm.com.IBM_cpu2msghub'
-
 #base64 $1 > image_in_bytes.txt
 
 curl -sS 'http://camera/api/get_snippet' > bytes.txt
